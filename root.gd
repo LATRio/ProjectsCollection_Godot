82fbdcfa7a742c75e7e0cb4183803c18c5a9fad2
LATRio <AlSenya@yandex.ru>
2026-09-19 13:17:48 +0900

    Add CMakePresets.json and export presets

--- root.gd
+++ root.gd
@@ -4,3 +4,6 @@ extends Control
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://TestProjects/Composition/Scenes/CompositionDemo.tscn")
+	# Compare gdscript vs gdextension performance. explore multiple scenarios?
+	# Compare node vs renderingserver performance. test incrementally each feature?
+	# maybe physics? node vs server?
